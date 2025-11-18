4a90c23f0fcfe85c7ad703e331c177cd74e97a0e
Nakada Tokumei <nakada_tokumei@protonmail.com>
2025-11-18 22:17:48 +0900

    [Develop] Add Default app main

---
+++ app.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/debrootfs/debapp"
+)
+
+func main() {
+	debapp.Main()
+}
---
+++ debapp/main.go
@@ -0,0 +1,8 @@
+package debapp
+
+import "fmt"
+
+func Main() {
+	fmt.Println("Hello makedebrootfs")
+	fmt.Println("This is go")
+}
