84cadc10aa076434a86dea903864d6e95bbf73d3
Nakada Tokumei <nakada_tokumei@protonmail.com>
2025-11-19 12:45:09 +0900

    [Develop] Add Default debootstrap workin

--- debapp/main.go
+++ debapp/main.go
@@ -1,8 +1,17 @@
 package debapp
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/debrootfs/util/bootstrap"
+)
 
 func Main() {
 	fmt.Println("Hello makedebrootfs")
 	fmt.Println("This is go")
+
+	bootstrap := bootstrap.DebBootstrap{}
+	if err := bootstrap.Create("amd64", "bookworm", "./debroot"); err != nil {
+		panic(err)
+	}
 }
--- util/bootstrap/bootstrap_tool.go
+++ util/bootstrap/bootstrap_tool.go
@@ -1,13 +1,34 @@
 package bootstrap
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 type DebBootstrap struct {
 	OutPath string
 }
 
 type DebBootstrapInterface interface {
-	Create(string)
+	Create(string, string, string)
 }
 
-func (d DebBootstrap) Create(path string) {
+func debootstrapVerbose(arch string, deb_ver string, path string) {
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	fmt.Println("Debian Version : " + deb_ver)
+	fmt.Println("Arch : " + arch)
+	fmt.Println("Output Path : " + path)
+	fmt.Println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+}
+
+func (d *DebBootstrap) Create(arch string, deb_ver string, path string) error {
+	debootstrapVerbose(arch, deb_ver, path)
+
 	d.OutPath = path
+	arch = "--arch=" + arch
+	cmd := exec.Command("sudo", "debootstrap", arch, deb_ver, path, "http://deb.debian.org/debian")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
